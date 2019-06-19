a9731a11bd12643d5af952a1714d5c87176c821b
alemoretto <a.moretto86@gmail.com>
2019-06-19 15:53:32 +0200

    primo commit

---
+++ src/it/prova/Destinatario.java
@@ -0,0 +1,71 @@
+package it.prova;
+
+public class Destinatario {
+
+	private String nome;
+	private String cognome;
+	private int eta;
+	private String indirizzo;
+	private boolean possessoreDiContoCorrente;
+	private PostaDiPaese postaDiPaese;
+	
+	public Destinatario(String nome, String cognome, int eta, String indirizzo, boolean possessoreDiContoCorrente,
+			PostaDiPaese postaDiPaese) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.eta = eta;
+		this.indirizzo = indirizzo;
+		this.possessoreDiContoCorrente = possessoreDiContoCorrente;
+		this.postaDiPaese = postaDiPaese;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public boolean isPossessoreDiContoCorrente() {
+		return possessoreDiContoCorrente;
+	}
+
+	public void setPossessoreDiContoCorrente(boolean possessoreDiContoCorrente) {
+		this.possessoreDiContoCorrente = possessoreDiContoCorrente;
+	}
+
+	public PostaDiPaese getPostaDiPaese() {
+		return postaDiPaese;
+	}
+
+	public void setPostaDiPaese(PostaDiPaese postaDiPaese) {
+		this.postaDiPaese = postaDiPaese;
+	}
+	
+}
---
+++ src/it/prova/PostaDiPaese.java
@@ -0,0 +1,63 @@
+package it.prova;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class PostaDiPaese {
+
+	private String denominazione;
+	private String indirizzoSede;
+	private Date dataApertura;
+	private int numeroDipendenti;
+	private List<Destinatario> destinatari = new ArrayList<>();
+	
+	public PostaDiPaese(String denominazione, String indirizzoSede, Date dataApertura, int numeroDipendenti) {
+		super();
+		this.denominazione = denominazione;
+		this.indirizzoSede = indirizzoSede;
+		this.dataApertura = dataApertura;
+		this.numeroDipendenti = numeroDipendenti;
+	}
+
+	public String getDenominazione() {
+		return denominazione;
+	}
+
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+
+	public String getIndirizzoSede() {
+		return indirizzoSede;
+	}
+
+	public void setIndirizzoSede(String indirizzoSede) {
+		this.indirizzoSede = indirizzoSede;
+	}
+
+	public Date getDataApertura() {
+		return dataApertura;
+	}
+
+	public void setDataApertura(Date dataApertura) {
+		this.dataApertura = dataApertura;
+	}
+
+	public int getNumeroDipendenti() {
+		return numeroDipendenti;
+	}
+
+	public void setNumeroDipendenti(int numeroDipendenti) {
+		this.numeroDipendenti = numeroDipendenti;
+	}
+
+	public List<Destinatario> getDestinatari() {
+		return destinatari;
+	}
+
+	public void setDestinatari(List<Destinatario> destinatari) {
+		this.destinatari = destinatari;
+	}
+	
+}
